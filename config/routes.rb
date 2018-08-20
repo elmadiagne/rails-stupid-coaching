14351f870959225dbb575b8b1240983533d28663
El hadji Diagne <elma.diagne@elmadeal.com>
2018-08-20 16:41:05 +0100

    coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'coaching#ask', as: :ask
+  get 'answer', to: 'coaching#answer', as: :answer
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
